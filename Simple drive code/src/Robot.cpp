bec00d323698ca6784c1f6991845965f66c7a3a6
ConnorCremers <connor.cremers@gmail.com>
2016-01-25 23:12:01 -0800

    Added nav x stuff

--- Simple drive code/src/Robot.cpp
+++ Simple drive code/src/Robot.cpp
@@ -18,7 +18,7 @@ class Robot: public SampleRobot
 	,lPow ,rPow;	//powers for drive train
 public:
 	Robot() :
-			lDrive(2) ,rDrive(3)
+			lDrive(2) ,rDrive(3) //declaring the ports of the motors/joystick
 			,stick(0)
 	{}
 	void robotinit(){
@@ -31,11 +31,11 @@ public:
 	{
 		while (IsOperatorControl() && IsEnabled())
 		{
-			throttle = adjust(stick.GetRawAxis(1));
-			turn = adjust(stick.GetRawAxis(4));
-			lPow = leftPower(throttle, turn);
-			rPow = rightPower(throttle, turn);
-			lDrive.Set(lPow);
+			throttle = adjust(stick.GetRawAxis(1));	//uses function adjust in adjustValues.cpp
+			turn = adjust(stick.GetRawAxis(4));	//it sets deadbands and rescales it
+			lPow = leftPower(throttle, turn);	//a fucntion for calculating drive power
+			rPow = rightPower(throttle, turn);	//it uses Omair's math, im not entirely sure what all it does
+			lDrive.Set(lPow);	//sets motor powers
 			rDrive.Set(rPow);
 			Wait(0.005);				// wait for a motor update time
 		}
