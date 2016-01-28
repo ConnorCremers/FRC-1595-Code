002ac0c357ad3d6f176404cd714a78ffc3243b6c
ConnorCremers <connor.cremers@gmail.com>
2016-01-27 16:52:08 -0800

    Declarations of most items

--- Blank template/src/Robot.cpp
+++ Blank template/src/Robot.cpp
@@ -7,14 +7,26 @@
  */
 class Robot : public SampleRobot
 {
-	float number;//need this so it doesnt have an error. Makes sense, right?
+	Joystick exampleJoystick;
+	CANTalon demoMotor;
+	DigitalInput exampleSwitch;
+	AnalogInput examplePotentiometer;
+	Solenoid exampleSolenoid;
+	DoubleSolenoid exampleDoubleSolenoid;
+	Encoder exampleEncoder = new Encoder(0, 1, false, Encoder::EncodingType::k4X);
 public:
 	Robot() :
-		number(3)
+		exampleJoystick(0)
+		,demoMotor(0)
+		,exampleSwitch(0)
+		,examplePotentiometer(0)
+		,exampleSolenoid(1, 0)
+		,exampleDoubleSolenoid(1, 1, 2)
+
 {}
 
 
-
+// [CDT GCC Built-in Compiler Settings] options
  void RobotInit(){
 	 //what to do when the robot first starts up (initialize sensors)
  }
